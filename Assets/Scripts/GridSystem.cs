b8c09624c0cb8f784b6038f6bf80fd7a78da7d2f
tiger <thisisstreamliner@gmail.com>
2022-08-01 16:35:22 +0300

    Grid Object

--- Assets/Scripts/GridSystem.cs
+++ Assets/Scripts/GridSystem.cs
@@ -4,35 +4,50 @@ using UnityEngine;
 
 public class GridSystem
 {
-    private int width;
-    private int height;
-    private float cellSize;
+    private int _width;
+    private int _height;
+    private float _cellSize;
+    private GridObject[,] _gridObjectArray; 
     
     public GridSystem(int width, int height, float cellSize)
     {
-        this.width = width;
-        this.height = height;
-        this.cellSize = cellSize;
+        _width = width;
+        _height = height;
+        _cellSize = cellSize;
 
-        for (int x = 0; x < width; x++)
+        _gridObjectArray = new GridObject[_width, _height];
+
+        for (int x = 0; x < _width; x++)
         {
-            for (int z = 0; z < height; z++)
+            for (int z = 0; z < _height; z++)
             {
-                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * .2f, Color.white, 1000);
+                GridPosition gridPosition = new GridPosition(x, z);
+                _gridObjectArray[x, z] = new GridObject(this, gridPosition);
             }
         }
     }
 
     public Vector3 GetWorldPosition(int x, int z)
     {
-        return new Vector3(x, 0, z) * cellSize;
+        return new Vector3(x, 0, z) * _cellSize;
     }
 
     public GridPosition GetGridPosition(Vector3 worldPosition)
     {
         return new GridPosition(
-            Mathf.RoundToInt(worldPosition.x / cellSize), 
-            Mathf.RoundToInt(worldPosition.z / cellSize)
+            Mathf.RoundToInt(worldPosition.x / _cellSize), 
+            Mathf.RoundToInt(worldPosition.z / _cellSize)
         );
+    }
+
+    public void CreateDebugObjects(Transform debugPrefab)
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int z = 0; z < _height; z++)
+            {
+                GameObject.Instantiate(debugPrefab, GetWorldPosition(x, z), Quaternion.identity);
+            }
+        }
     }
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField] private Transform gridDebugObjectPrefab;
     private GridSystem _gridSystem;
     private void Start()
     {
         _gridSystem = new GridSystem(10, 10, 2f);
+        _gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
     }
 
     private void Update()
